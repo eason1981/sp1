8ff795b3c67c7f88a5693c7bdb002bc04feb633a
eason <eason@brevis.network>
2024-09-03 17:03:29 +0800

    Update fib script code.

--- examples/fibonacci/script/src/main.rs
+++ examples/fibonacci/script/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     utils::setup_logger();
 
     // Create an input stream and write '500' to it.
-    let n = 1000u32;
+    let n = 10_u32;
 
     // The input stream that the program will read from using `sp1_zkvm::io::read`. Note that the
     // types of the elements in the input stream must match the types being read in the program.
@@ -19,43 +19,42 @@ fn main() {
     let client = ProverClient::new();
 
     // Execute the program using the `ProverClient.execute` method, without generating a proof.
+    /*
     let (_, report) = client.execute(ELF, stdin.clone()).run().unwrap();
     println!(
         "executed program with {} cycles",
         report.total_instruction_count()
     );
+    */
 
     // Generate the proof for the given program and input.
     let (pk, vk) = client.setup(ELF);
     let mut proof = client.prove(&pk, stdin).run().unwrap();
-
     println!("generated proof");
 
-    // Read and verify the output.
-    //
-    // Note that this output is read from values commited to in the program using
-    // `sp1_zkvm::io::commit`.
-    let _ = proof.public_values.read::<u32>();
-    let a = proof.public_values.read::<u32>();
-    let b = proof.public_values.read::<u32>();
+    /*
+        // Read and verify the output.
+        //
+        // Note that this output is read from values commited to in the program using
+        // `sp1_zkvm::io::commit`.
+        let _ = proof.public_values.read::<u32>();
+        let a = proof.public_values.read::<u32>();
+        let b = proof.public_values.read::<u32>();
 
-    println!("a: {}", a);
-    println!("b: {}", b);
+        println!("a: {}", a);
+        println!("b: {}", b);
 
-    // Verify proof and public values
-    client.verify(&proof, &vk).expect("verification failed");
+        // Verify proof and public values
+        client.verify(&proof, &vk).expect("verification failed");
 
-    // Test a round trip of proof serialization and deserialization.
-    proof
-        .save("proof-with-pis.bin")
-        .expect("saving proof failed");
-    let deserialized_proof =
-        SP1ProofWithPublicValues::load("proof-with-pis.bin").expect("loading proof failed");
+        // Test a round trip of proof serialization and deserialization.
+        proof.save("proof-with-pis.bin").expect("saving proof failed");
+        let deserialized_proof =
+            SP1ProofWithPublicValues::load("proof-with-pis.bin").expect("loading proof failed");
 
-    // Verify the deserialized proof.
-    client
-        .verify(&deserialized_proof, &vk)
-        .expect("verification failed");
+        // Verify the deserialized proof.
+        client.verify(&deserialized_proof, &vk).expect("verification failed");
 
-    println!("successfully generated and verified proof for the program!")
+        println!("successfully generated and verified proof for the program!");
+    */
 }
